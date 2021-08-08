72226b22dc40bc1f1bb809fce49190db193d2178
thakaj <taulant.hakaj@scranton.edu>
2021-08-08 14:05:04 -0400

    commit tree 1deaa658ea9a0abedac2f1e314c09af55bbf236f

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+    def actor
+        self.characters
+    end
+    def show
+        self.shows
+    end
+    def say_that_thing_you_say
+       "#{self.characters} #{self.catchphrase}" 
+    end
 end
--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,6 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    create_column :characters, :catchphrase, :string
+    end
   end
 end
